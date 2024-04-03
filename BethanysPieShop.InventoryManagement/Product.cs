b9a7e7f76bbe8b1317228b2fb0bf64d723477d04
Maria Moraru <morarumaria1988@gmail.com>
2024-04-03 16:37:49 +0200

    Add project files.

---
+++ BethanysPieShop.InventoryManagement/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.InventoryManagement
+{
+    public class Product
+    {
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+    }
+}
